e6449846ddeb471bcfe3398376cd11a51d76411f
dsasank04 <d.sasank18@gmail.com>
2023-04-26 13:39:40 +0530

    Inserted Bottom Navigation Bar and Message Inbox(i.e SMS Log) and deleted 2 extra dart files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:call_log/call_log.dart';
+import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,17 +13,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<List<CallLogEntry>> _callLogsFuture;
+  final SmsQuery _query = SmsQuery();
+  List<SmsMessage> _messages = [];
+  List<CallLogEntry> _callLogs = [];
+
+  int _currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _callLogsFuture = _getCallLogs();
+    _getCallLogs();
   }
 
-  Future<List<CallLogEntry>> _getCallLogs() async {
+  Future<void> _getCallLogs() async {
     var result = await CallLog.query();
-    return result.toList();
+    setState(() => _callLogs = result.toList());
+  }
+
+  Future<void> _getSmsMessages() async {
+    var permission = await Permission.sms.status;
+    if (permission.isGranted) {
+      final messages = await _query.querySms(
+        kinds: [SmsQueryKind.inbox, SmsQueryKind.sent],
+        count: 10,
+      );
+      debugPrint('sms inbox messages: ${messages.length}');
+      setState(() => _messages = messages);
+    } else {
+      await Permission.sms.request();
+    }
   }
 
   @override
@@ -30,37 +50,78 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Call Logs'),
+          title: const Text('Call Logs & SMS Inbox'),
         ),
-        body: FutureBuilder(
-          future: _callLogsFuture,
-          builder: (BuildContext context, AsyncSnapshot<List<CallLogEntry>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
+        body: _getBody(),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() => _currentIndex = index);
+            if (index == 1) {
+              _getSmsMessages();
             }
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('Failed to load call logs.'),
-              );
-            }
-            final callLogs = snapshot.data!;
-            return ListView.builder(
-              itemCount: callLogs.length,
-              itemBuilder: (BuildContext context, int index) {
-                final callLog = callLogs[index];
-                return ListTile(
-                  leading: const Icon(Icons.call),
-                  title: Text(callLog.name ?? callLog.number ?? ''),
-                  subtitle: Text(
-                    '${callLog.callType}',
-                  ),
-                );
-              },
-            );
           },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Call Logs',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: 'SMS Inbox',
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _getBody() {
+    switch (_currentIndex) {
+      case 0:
+        return _buildCallLogs();
+      case 1:
+        return _buildSmsMessages();
+      default:
+        return Container();
+    }
+  }
+
+  Widget _buildCallLogs() {
+    if (_callLogs.isEmpty) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return ListView.builder(
+      itemCount: _callLogs.length,
+      itemBuilder: (BuildContext context, int index) {
+        final callLog = _callLogs[index];
+        return ListTile(
+          leading: const Icon(Icons.call),
+          title: Text(callLog.name ?? callLog.number ?? ''),
+          subtitle: Text(
+            '${callLog.callType}',
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildSmsMessages() {
+    return Container(
+      padding: const EdgeInsets.all(0.0),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: _messages.length,
+        itemBuilder: (BuildContext context, int i) {
+          var message = _messages[i];
+          return ListTile(
+            leading: const Icon(Icons.message_rounded),
+            title: Text('${message.sender} [${message.date}]'),
+            subtitle: Text('${message.body}'),
+          );
+        },
       ),
     );
   }
